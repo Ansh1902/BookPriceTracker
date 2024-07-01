6714e4f4d7e6dd8c4c578aa8d55c2a28df3182ad
Ansh Kharbanda <78063224+Ansh1902@users.noreply.github.com>
2024-07-01 13:03:37 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 # constants.py
 from datetime import datetime
-USERNAME = 'Anshk2002.ak@gmail.com'
-PASSWORD = 'Ansh1902'
+USERNAME = '**'
+PASSWORD = '**'
 DATE = datetime.now().strftime("%d_%m_%y")  # Use the current date
 PRICE_DROP_PERCENTAGE = 5  # 5% price drop
